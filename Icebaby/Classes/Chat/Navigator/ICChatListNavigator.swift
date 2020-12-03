999f9d0b4c6b153203e1a4e0251bed55439c936d
Henry.Liao <Henry.Liao@skylinetw.com>
2020-12-03 12:51:03 +0800

    chat list 功能基礎架構

---
+++ Icebaby/Classes/Chat/Navigator/ICChatListNavigator.swift
@@ -0,0 +1,12 @@
+//
+//  ICChatListNavigator.swift
+//  Icebaby
+//
+//  Created by Henry.Liao on 2020/12/3.
+//
+
+import UIKit
+
+class ICChatListNavigator: NSObject {
+
+}
---
+++ Icebaby/Classes/Chat/ViewModel/ICChatListViewMocel.swift
@@ -0,0 +1,12 @@
+//
+//  ICChatListViewMocel.swift
+//  Icebaby
+//
+//  Created by Henry.Liao on 2020/12/3.
+//
+
+import UIKit
+
+class ICChatListViewMocel: NSObject {
+
+}
