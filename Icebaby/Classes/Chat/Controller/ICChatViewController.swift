182589d5a1a6b7670dd517041b88bc9761667a9a
henry <toyokoyo199@gmail.com>
2020-12-15 01:32:20 +0800

    調整聊天畫面UI

--- Icebaby/Classes/Chat/Controller/ICChatViewController.swift
+++ Icebaby/Classes/Chat/Controller/ICChatViewController.swift
@@ -10,7 +10,34 @@ import MessageKit
 
 class ICChatViewController: MessagesViewController {
 
-    
+    private var messages: [MessageType] = {
+        
+        var messages: [MessageType] = []
+        
+        let sender1 = ICSender()
+        sender1.displayName = "Henry"
+        sender1.senderId = "10001"
+        
+        let sender2 = ICSender()
+        sender2.displayName = "Jeff"
+        sender2.senderId = "10002"
+        
+        let msg1 = ICMessage()
+        msg1.messageId = "1"
+        msg1.kind = .text("你好喔!!!")
+        msg1.sentDate = Date()
+        msg1.sender = sender1
+        
+        let msg2 = ICMessage()
+        msg2.messageId = "2"
+        msg2.kind = .text("HI~~~~~~~~~")
+        msg2.sentDate = Date()
+        msg2.sender = sender2
+        
+        messages.append(msg1)
+        messages.append(msg2)
+        return messages
+    }()
 
 }
 
@@ -18,24 +45,45 @@ class ICChatViewController: MessagesViewController {
 extension ICChatViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-//        messagesCollectionView.messagesDataSource = self
-//        messagesCollectionView.messagesLayoutDelegate = self
-//        messagesCollectionView.messagesDisplayDelegate = self
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+
     }
 }
 
-//extension ICChatViewController: MessagesDataSource {
-//    func currentSender() -> SenderType {
-//        <#code#>
-//    }
-//    
-//    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
-//        
-//    }
-//    
-//    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
-//        return 0
-//    }
-//    
-//}
+extension ICChatViewController: MessagesDataSource {
+    func currentSender() -> SenderType {
+        let sender = ICSender()
+        sender.displayName = "Henry"
+        sender.senderId = "10001"
+        return sender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
+    func messageTopLabelAttributedText(for message: MessageType, at indexPath: IndexPath) -> NSAttributedString? {
+        let name = message.sender.displayName
+        return NSAttributedString(string: name, attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15)])
+    }
+    
+}
+
+extension ICChatViewController: MessagesDisplayDelegate {
+    
+}
+
+
+extension ICChatViewController: MessagesLayoutDelegate {
+    func messageTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        return 20
+    }
+}
 
