b87b82feac6aff3ed898892aa0c9095760bc9296
Henry.Liao <Henry.Liao@skylinetw.com>
2020-12-24 21:01:26 +0800

    串接 更新 read date api

--- Icebaby/API/URLProtocol.swift
+++ Icebaby/API/URLProtocol.swift
@@ -21,35 +21,7 @@ protocol ICLobbyURL: ICBaseURL {
 protocol ICChatURL: ICBaseURL {
     var newChatURL: URL { get }
     var myChannelsURL: URL { get }
-}
-
-
-protocol ICCourtURL: ICBaseURL {
-    var getLocationsURL: URL { get }
-    var getLocationDetailURL: URL { get }
-    var checkin: URL { get }
-    var currentCheckin: URL { get }
-    var getFavoriteCourtURL: URL { get }
-    var inCourtRange: URL { get }
-}
-protocol ICCourtDetailURL: ICBaseURL {
-    var getCourtDetailURL: URL { get }
-    var addFavoriteURL: URL { get }
-    var deleteFavoriteURL: URL { get }
-}
-protocol ICUserURL: ICBaseURL {
-    var userDetailURL: URL { get }
-    var editUserDetailURL: URL { get }
-    var editUserImageURL: URL { get }
-}
-protocol ICMessageURL: ICBaseURL {
-    var getMessageURL: URL { get }
-    var addMessageURL: URL { get }
-}
-protocol ICFavoriteURL: ICBaseURL {
-    var getFavoriteCourtURL: URL { get }
-    var addFavoriteURL: URL { get }
-    var deleteFavoriteURL: URL { get }
+    func updateMemberURL(memberID: Int) -> URL
 }
 
 
@@ -83,62 +55,8 @@ extension ICChatURL {
     var myChannelsURL: URL {
         return URL(string: "\(baseURL)/icebaby/v1/chat/mychannels")!
     }
-}
-
-
-extension ICCourtURL {
-    var getLocationsURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/GetLocations")!
-    }
-    var getLocationDetailURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/GetLocationDetail")!
-    }
-    var checkin: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/Checkin")!
-    }
-    var currentCheckin: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/User/GetSelfCurrentCheckinCourt")!
-    }
-    var findCourt: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/FindNotice")!
-    }
-    var getFavoriteCourtURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/User/GetSelfFavoriteCourt")!
-    }
-    var inCourtRange: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/InCourtRange")!
-    }
-}
-extension ICCourtDetailURL {
-    var getCourtDetailURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/GetDetail")!
-    }
-    var addFavoriteURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/AddFavorite")!
-    }
-    var deleteFavoriteURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/DeleteFavorite")!
-    }
-}
-extension ICUserURL {
-    var userDetailURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/User/GetSomeoneDetail")!
-    }
-    var editUserDetailURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/User/EditSelfDetail")!
-    }
-    var editUserImageURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/User/UploadSelfImage")!
-    }
-}
-extension ICMessageURL {
-    var getMessageURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/GetMessage")!
-    }
-    var addMessageURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/AddMessage")!
-    }
-    var deleteMessageURL: URL {
-        return URL(string: "\(baseURL)/WebBridge/api/Court/DeleteMessage")!
+    
+    func updateMemberURL(memberID: Int) -> URL {
+        return URL(string: "\(baseURL)/icebaby/v1/chat/member/\(memberID)")!
     }
 }
--- Icebaby/Service/ICChatAPIService.swift
+++ Icebaby/Service/ICChatAPIService.swift
@@ -15,6 +15,7 @@ import Alamofire
 protocol ICChatAPI {
     func apiNewChat(guestID: Int) -> Single<String?>
     func apiGetMyChannel() -> Single<[ICChannel]>
+    func apiUpdateReadDate(memberID: Int, date: Date) -> Single<ICMember?>
 }
 
 class ICChatAPIService: APIBaseRequest, APIDataTransform, ICChatAPI, ICChatURL {
@@ -49,6 +50,23 @@ class ICChatAPIService: APIBaseRequest, APIDataTransform, ICChatAPI, ICChatURL {
                 .map ({ (result) -> [ICChannel] in
                     let data = result.dictionary?["data"]?.array ?? []
                     return self.dataDecoderArrayTransform(ICChannel.self, data)
+                })
+                .subscribe(onSuccess: { (channels) in
+                    single(.success(channels))
+                }, onError: { (error) in
+                    single(.error(error))
+                })
+            return Disposables.create()
+        }
+    }
+    
+    func apiUpdateReadDate(memberID: Int, date: Date) -> Single<ICMember?> {
+        let header = HTTPHeaders(["token": self.userManager.token() ?? ""])
+        return Single<ICMember?>.create { [unowned self] (single) -> Disposable in
+            let _ = self.sendRequest(medthod: .get, url: self.updateMemberURL(memberID: self.userManager.uid()), parameter: nil, headers: header)
+                .map ({ (result) -> ICMember? in
+                    let data = result.dictionary?["data"] ?? JSON()
+                    return self.dataDecoderTransform(ICMember.self, data)
                 })
                 .subscribe(onSuccess: { (channels) in
                     single(.success(channels))
